890fb3b4fd6a6d0992085a4875fecd02a92627e0
jhpark <pjho123456@gmail.com>
2020-03-13 16:53:29 +0900

    before pc change

--- experimental/notepad.py
+++ experimental/notepad.py
@@ -1,285 +1,221 @@
-from data_accessors.FtpAccessor import FtpAccessor
-from nwp_object.FilesContainer import FilesContainer
-from nwp_object.NwpFile import *
-from util.input_converter import *
+import multiprocessing
+import datetime
+import pandas as pd
+import pygrib
+import os
+import sys
+sys.path.append('/home/jhpark/PycharmProjects/NWPparsing')
 import CONSTANT
-import time
+from util.NwpGridAnalyzer import NwpGridAnalyzer
+from nwp_object import NwpFile
+from util.input_converter import InputConverter
 
 
-# time_interval = [2019080100, 2019091000]
-# time_interval = InputConverter().time_interval_conversion(time_interval)
-#
-# accessor = FtpAccessor(CONSTANT.ftp_ip, CONSTANT.ftp_id,
-#                                         CONSTANT.ftp_pw)
-# container = FilesContainer(LdapsFile, "unis", [(33.2875, 126.648611)],
-#                            ["NDNSW"])
-# container.generate_base_files(time_interval)
-#
-# filename = "l015_v070_erlo_unis_h002.2019042306.gb2"
-# result = accessor.existence_check(filename)
-# print(result)
+@profile
+def extract_value(nearest_type=1):
+    df = None
 
-# container = FilesContainer(LdapsFile, "unis", [(33.2875, 126.648611)],
-#                            ["NDNSW"])
-# container.generate_real_time_prediction_files(accessor)
-#
-# print(container.filename_list)
-# print(len(container.filename_list))
+    def base_setting(grid_analyzer):
+        info_file_name = CONSTANT.ldaps_variable_index_file_name
+        nwp_var_info = pd.read_excel(
+            os.path.join(CONSTANT.setting_file_path, info_file_name))
+        nwp_var_index_dic = nwp_var_info.set_index(
+            "var_abbrev")["index"]
+        if grid_analyzer.set_lat_lon_call % 100 == 0:
+            lat_lon_grid = pygrib_file[1].latlons()
 
-# import CONSTANT
-# import pandas as pd
-# a = pd.read_excel(CONSTANT.data_file_path + "before_refactor_df.xlsx")
-# b = pd.read_excel(CONSTANT.data_file_path + "after_refactor_df.xlsx")
-# c = a.equals(b)
-# print(c)
+            lat = lat_lon_grid[0]
+            lon = lat_lon_grid[1]
+            grid_analyzer.set_lat_lon_grid(lat, lon)
+
+            # grid_analyzer.dset_lat_lon_grid(
+            #     pygrib_file[1].latlons()[0], pygrib_file[1].latlons()[1])
+        return nwp_var_index_dic
+
+    file = NwpFile.LdapsFile("unis", 13,
+                             InputConverter.current_time_conversion(
+                                 2019121300),
+                             CONSTANT.jenon_coordinates)
+
+    grid_analyzer = NwpGridAnalyzer()
+
+    pygrib_file = pygrib.open(os.path.join(CONSTANT.files_path,
+                                           "l015_v070_erlo_unis_h039.2019100112.gb2"))
+    nwp_var_index_dic = base_setting(grid_analyzer)
+    crtn_tm = file.crtn_tm + datetime.timedelta(hours=9)
+    fcst_tm = file.fcst_tm + datetime.timedelta(hours=9)
+    row_num = len(file.location_points)
+
+    df = pd.DataFrame()
+    crtn_column = [crtn_tm for i in range(row_num)]
+    df["CRTN_TM"] = crtn_column
+    df["horizon"] = file.horizon
+    df["FCST_TM"] = fcst_tm
+    df["lat"] = [point[0] for point in file.location_points]
+    df["lon"] = [point[1] for point in file.location_points]
+    df["location_num"] = [i for i in range(len(file.location_points))]
+
+    if file.variables == "all":
+        file.variables = pd.read_excel(CONSTANT.setting_file_path + file.info_file_name).set_index(
+            "index")["var_abbrev"].to_list()
+
+    value_dic = {}
+    for i, var_name in enumerate(file.variables):
+        var_index_inside_pygrib_file = nwp_var_index_dic[var_name]. \
+            item()
+        value_array = pygrib_file[var_index_inside_pygrib_file].values
+        value_dic[var_name] = value_array
+
+    nearest_index_dic = {}
+    for i, point in enumerate(file.location_points):
+        if nearest_type != 1:
+            nearest_point_dis_index_dic, _ = \
+                grid_analyzer.nearest_n_grid_point(
+                    point[0], point[1], nearest_type)
+            nearest_index_dic[i] = nearest_point_dis_index_dic
+            # value = grid_analyzer. \
+            #     nearest_n_point_weighted_value(
+            #     value_array, nearest_point_dis_index_dic)
+        else:
+            nearest_point_index = grid_analyzer. \
+                nearest_point_index(point[0], point[1])
+            nearest_index_dic[i] = nearest_point_index
+            # value = value_array[nearest_point_index[0]][
+            #     nearest_point_index[1]]
+
+    for i, var_name in enumerate(file.variables):
+        # var_index_inside_pygrib_file = nwp_var_index_dic[var_name]. \
+        #     item()
+        # value_array = pygrib_file[var_index_inside_pygrib_file].values
+        value_array = value_dic[var_name]
+        value_list = []
+        for i, point in enumerate(file.location_points):
+            if nearest_type != 1:
+                # nearest_point_dis_index_dic, _ = \
+                #     grid_analyzer.nearest_n_grid_point(
+                #         point[0], point[1], nearest_type)
+                value = grid_analyzer.nearest_n_point_weighted_value(
+                    value_array, nearest_index_dic[i])
+                # value = grid_analyzer. \
+                #     nearest_n_point_weighted_value(
+                #     value_array, nearest_point_dis_index_dic)
+                value_list.append(value)
+            else:
+                # nearest_point_index = grid_analyzer. \
+                #     nearest_point_index(point[0], point[1])
+                # value = value_array[nearest_point_index[0]][
+                #     nearest_point_index[1]]
+                value = value_array[nearest_index_dic[i][0],
+                                    nearest_index_dic[i][1]]
+                value_list.append(value)
+        df[var_name] = value_list
+    pygrib_file.close()
+
+    return df
 
 
-# def decorator(func):
-#     print("wow")
-#
-#     def deco(*args):
-#         print(*args)
+@profile
+def extract_value2(nearest_type=1):
+    df = None
 
-    # print(func)
+    def base_setting(grid_analyzer):
+        info_file_name = CONSTANT.ldaps_variable_index_file_name
+        nwp_var_info = pd.read_excel(
+            os.path.join(CONSTANT.setting_file_path, info_file_name))
+        nwp_var_index_dic = nwp_var_info.set_index(
+            "var_abbrev")["index"]
+        if grid_analyzer.set_lat_lon_call % 100 == 0:
+            lat_lon_grid = pygrib_file[1].latlons()
 
-    # return deco
+            lat = lat_lon_grid[0]
+            lon = lat_lon_grid[1]
+            grid_analyzer.set_lat_lon_grid(lat, lon)
+
+            grid_analyzer.set_lat_lon_grid(
+                pygrib_file[1].latlons()[0], pygrib_file[1].latlons()[1])
+        return nwp_var_index_dic
+
+    file = NwpFile.LdapsFile("unis", 13,
+                             InputConverter.current_time_conversion(
+                                 2019121300),
+                             CONSTANT.jenon_coordinates)
+
+    grid_analyzer = NwpGridAnalyzer()
+
+    pygrib_file = pygrib.open(os.path.join(CONSTANT.files_path,
+                                           "l015_v070_erlo_unis_h039.2019100112.gb2"))
+    nwp_var_index_dic = base_setting(grid_analyzer)
+    crtn_tm = file.crtn_tm + datetime.timedelta(hours=9)
+    fcst_tm = file.fcst_tm + datetime.timedelta(hours=9)
+    row_num = len(file.location_points)
+
+    df = pd.DataFrame()
+    crtn_column = [crtn_tm for i in range(row_num)]
+    df["CRTN_TM"] = crtn_column
+    df["horizon"] = file.horizon
+    df["FCST_TM"] = fcst_tm
+    df["lat"] = [point[0] for point in file.location_points]
+    df["lon"] = [point[1] for point in file.location_points]
+    df["location_num"] = [i for i in range(len(file.location_points))]
+
+    if file.variables == "all":
+        file.variables = pd.read_excel(CONSTANT.setting_file_path + file.info_file_name).set_index(
+            "index")["var_abbrev"].to_list()
+
+    # value_dic = {}
+    # for i, var_name in enumerate(file.variables):
+    #     var_index_inside_pygrib_file = nwp_var_index_dic[var_name]. \
+    #         item()
+    #     value_array = pygrib_file[var_index_inside_pygrib_file].values
+    #     value_dic[var_name] = value_array
+    #
+    # nearest_index_dic = {}
+    # for i, point in enumerate(file.location_points):
+    #     if nearest_type != 1:
+    #         nearest_point_dis_index_dic, _ = \
+    #             grid_analyzer.nearest_n_grid_point(
+    #                 point[0], point[1], nearest_type)
+    #         nearest_index_dic[i] = nearest_point_dis_index_dic
+    #         # value = grid_analyzer. \
+    #         #     nearest_n_point_weighted_value(
+    #         #     value_array, nearest_point_dis_index_dic)
+    #     else:
+    #         nearest_point_index = grid_analyzer. \
+    #             nearest_point_index(point[0], point[1])
+    #         nearest_index_dic[i] = nearest_point_index
+    #         # value = value_array[nearest_point_index[0]][
+    #         #     nearest_point_index[1]]
+
+    for i, var_name in enumerate(file.variables):
+        var_index_inside_pygrib_file = nwp_var_index_dic[var_name]. \
+            item()
+        value_array = pygrib_file[var_index_inside_pygrib_file].values
+        # value_array = value_dic[var_name]
+        value_list = []
+        for i, point in enumerate(file.location_points):
+            if nearest_type != 1:
+                nearest_point_dis_index_dic, _ = \
+                    grid_analyzer.nearest_n_grid_point(
+                        point[0], point[1], nearest_type)
+                # value = grid_analyzer.nearest_n_point_weighted_value(
+                #     value_array, nearest_index_dic[i])
+                value = grid_analyzer. \
+                    nearest_n_point_weighted_value(
+                    value_array, nearest_point_dis_index_dic)
+                value_list.append(value)
+            else:
+                nearest_point_index = grid_analyzer. \
+                    nearest_point_index(point[0], point[1])
+                value = value_array[nearest_point_index[0]][
+                    nearest_point_index[1]]
+                # value = value_array[nearest_index_dic[i][0],
+                #                     nearest_index_dic[i][1]]
+                value_list.append(value)
+        df[var_name] = value_list
+    pygrib_file.close()
+
+    return df
 
 
-# @decorator
-# def original_func(a, b):
-#     print("original", a, b)
-#
-#
-# class TestClass:
-#     def __init__(self):
-#         print("start")
-#
-#     @classmethod
-#     def ab(cls):
-#         print("classmethoduse")
-#
-#
-#
-# def a(a):
-#     print("start")
-#     1/0
-#
-# def b(a):
-#     a(a)
-#
-# def c(a):
-#     b(a)
-#
-# def d(a):
-#     c(a)
-#
-# def e(a):
-#     d(a)
-#
-# def f(a):
-#     e(a)
-#
-# def g(a):
-#     f(a)
-#
-# def h(a):
-#     g(a)
-#
-# def i(a):
-#     h(a)
-#
-# def j(a):
-#     i(a)
-#
-# def k(a):
-#     j(a)
-#
-# def l(a):
-#     k(a)
-#
-# def m(a):
-#     l(a)
-#
-# def n(a):
-#     m(a)
-#
-#
-#
-#
-#
-#
-# a = {}
-# a["a"] = 1
-# # a["a"] = 2
-#
-# print(a)
-#
-# import datetime
-#
-# key = lambda timestamp, value: datetime.date.fromtimestamp(timestamp)
-# print(type(key))
-# from itertools import *
-#
-# a = [1,2,3,4]
-# b = ["a","b","c","d"]
-# c = [0,1,2,3]
-#
-# d = chain(a,b,c)
-# print(list(d))
-#
-
-# def a():
-#     msg = "how"
-#     def b():
-#         def c():
-#             def d():
-#                 msg = "wow"
-#                 def e():
-#                     def f():
-#                         def g():
-#                             print(msg)
-#
-#                         g()
-#
-#
-# def wow():
-#     print("who")
-#
-#     def how():
-#         print("how")
-#
-#     how()
-#
-#
-#
-# a = [1,2,3,4]
-# print(min(a))
-
-# def outer(a, b):
-#     c = a
-#     d = b
-#     def inner():
-#         return c, d
-#     return inner
-#
-# func = outer(1, 2)
-# func2 = outer(3, 4)
-#
-# print(func())
-# print(func2())
-#
-#
-#
-# import sys
-# from util.input_converter import *
-#
-# sys_arg = sys.argv
-#
-# a = 201911190012
-# current_time = InputConverter().current_time_conversion_12char(a)
-# print(current_time)
-
-# import pandas as pd
-# import CONSTANT
-#
-# a = pd.read_excel(CONSTANT.setting_file_path + "var_by_model_info.xlsx")
-# b = a["rdaps.h5"][0].replace(" ", "").split(",")
-# # print(list(a[a.model_name == "rdaps.h5"]["var"]))
-# c = a["rdaps.h5"]
-# print(c)
-# print(b)
-#
-# print(type(b))
-#
-# for i in b:
-#     print(i)
-
-# class A:
-#     c = 10
-#
-#     def __init__(self, ssn):
-#         self.v1 = 1
-#         self.v2 = 2
-#         self.c += 20
-#
-#     def m1(self):
-#         pass
-#
-#     def m2(self):
-#         pass
-#
-#     @classmethod
-#     def c1(cls):
-#         cls.c += 20
-#         pass
-#
-#
-# import logging
-#
-#
-# mylogger = logging.getLogger("my")
-#
-#
-# mylogger.setLevel(logging.INFO)
-#
-# stream_handler = logging.StreamHandler()
-# mylogger.addHandler(stream_handler)
-# mylogger.info("server start!")
-#
-
-# import logging
-# import notepad2
-#
-#
-#
-# logging.basicConfig(level=logging.CRITICAL, filename="exp.log")
-#
-# "----------------------"
-# logger = logging.getLogger("mylogger")
-# logger.setLevel(logging.DEBUG)
-#
-# loggerchild = logger.getChild("mychildlogger")
-#
-# print(loggerchild)
-# loggergrandchild = loggerchild.getChild("mygrandchild")
-# print(loggergrandchild)
-#
-# fh = logging.FileHandler("tormentalist.log")
-# fh.setLevel(logging.DEBUG)
-# loggergrandchild.addHandler(fh)
-# logger.addHandler(fh)
-#
-# formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %("
-#                               "message)s")
-# fh.setFormatter(formatter)
-#
-# loggergrandchild.debug("1")
-# loggergrandchild.info("green")
-# loggergrandchild.warning("warn")
-# loggergrandchild.error("error ocurred")
-# logger.critical("omg")
-#
-# print(logger)
-#
-# notepad2.log_sth()
-
-# from util.Visualizer import Visualizer
-#
-# a = Visualizer()
-# b = [(37.246238, 127.7247), (37.246268, 127.725193), (37.246432, 127.724826),
-#      (37.246545, 127.725303), (37.24672, 127.72493), (37.246754, 127.725389),
-#      (37.247061, 127.725099),(37.247154, 127.725493),
-# (37.247381, 127.725206),
-# (37.247364, 127.725603),
-# (37.247233, 127.725866),
-# (37.247081, 127.726307),
-# (37.24706, 127.72584),
-# (37.246857, 127.726213),
-# (37.24679, 127.725793),
-# (37.246585, 127.726159),
-# (37.246512, 127.725751),
-# (37.24639, 127.726076),
-# (37.246283, 127.725615),
-# (37.246108, 127.726017)]
-#
-# a.plot_some_points(b)
+extract_value()
+extract_value2()
--- experimental/notepad0.py
+++ experimental/notepad0.py
@@ -1,81 +1,46 @@
-# import datetime
-#
-# a = datetime.datetime.now().replace(microsecond=0)
-# b = datetime.datetime.now().replace(microsecond=0)
-#
-# print(int((b-a).total_seconds()))
-
-from controllers.prediction_maker import GeneralPredictionMaker
-from data_accessors.MongoDbConnector import get_site_info_df
-from util.input_converter import InputConverter
-from controllers.real_data_maker import VppRealMaker
-import pandas as pd
-import numpy as np
-import math
+import os
 import CONSTANT
-# prediction_maker = TimeDesignatedPredictionMaker()
-
-checkpoint = False
+import pandas as pd
 
 
-if checkpoint is not True:
-    # set site list
-    site_info = get_site_info_df()
-    id_list = list(site_info[site_info.capacity == 99]["COMPX_ID"])
+def merge_files_by_horizontally():
+    root_path = CONSTANT.data_file_path + "/merge/"
+    filename_list = os.listdir(root_path)
 
-    # make prediction & real data
-    setting = ["unis", InputConverter().vpp_compx_id_to_coordinates(
-        id_list, get_site_info_df()), "nonje_1016model.h5", "rdaps.h5"]
-    time_interval = [2019100106, 2019100406]
-    prediction_maker = GeneralPredictionMaker(*setting)
-    real_maker = VppRealMaker(time_interval, id_list)
+    while True:
+        filename = filename_list.pop()
+        if filename[11] == "s":
+            df = pd.read_excel(root_path + filename)
+            break
 
-    df = prediction_maker.create_interval_prediction("regularly", time_interval)
-    real = real_maker.query_real_data()
+    for filename in filename_list:
+        if filename[11] == "s":
+            df = df.append(pd.read_excel(root_path + filename))
+    return df
 
-    # join with key
-    merged = pd.merge(df, real, how="left", on=["FCST_TM", "location_num"])
-    merged = merged.drop(columns=["lat_x", "lon_x", "lat_y", "lon_y"])
-
-    # filter mergerd df
-    # merged = merged[merged.production > 0]
-    # merged = merged[merged.horizon == 6]
-
-    # make criteria column
-    merged["rmse"] = (merged["production"] - merged["prediction"])**2
-    merged["mape"] = abs(merged["production"] - merged["prediction"])
-    merged["mbe"] = merged["production"] - merged["prediction"]
-
-    merged.to_excel(CONSTANT.data_file_path + "0106test.xlsx")
-else:
-    merged = pd.read_excel(CONSTANT.data_file_path + "0106test.xlsx")
-
-# group by horizon
-merged = merged.groupby(merged["horizon"])
-merged = merged.agg(["count", "mean", "sum"])
-
-merged["rRMSE"] = (merged["rmse"]["sum"].apply(np.sqrt))/(merged[
-                                                            "production"][
-                                                            "mean"] * merged[
-                                                            "production"][
-                                                            "count"].apply(
-                                                            np.sqrt))
-merged["rMBE"] = merged["mbe"]["sum"] / (merged["production"]["mean"] *
-                                         merged["production"]["count"])
-
-
-merged.to_excel(CONSTANT.data_file_path + "merged0106testt.xlsx")
-# merged = merged.mean()
+# for filename in filename_list:
+#     df = pd.read_excel(root_path + filename)
+#     print(df)
+# df1 = pd.read_excel(root_path + filename_list[0])
+# df2 = pd.read_excel(root_path + filename_list[1])
+# merged = df1.append(df2)
 # print(merged)
 
-# aggregate from criteria column
-# sum_error_square = merged["rmse"].sum()
-# rownum = merged.shape[0]
-# mean_production = merged["production"].mean()
 
-# rRMSE = (math.sqrt(sum_error_square)/(mean_production*math.sqrt(rownum)))*100
-# nMAPE = merged["mape"].mean()/99
-# rMBE = (merged["mbe"].sum()/(rownum*mean_production))*100
+df = merge_files_by_horizontally().drop(columns=["Unnamed: 0"])
+df.to_excel(CONSTANT.data_file_path + "{}.xlsx".
+                    format("seoulgarage_merged_6to12"))
 
-# print output
-# print(rRMSE, nMAPE, rMBE)
+# root_path = CONSTANT.data_file_path + "/merge/"
+# filename_list = os.listdir(root_path)
+#
+# print(filename_list)
+#
+# print(filename_list[0][11])
+# print(filename_list[1][11])
+# print(filename_list[2][11])
+# print(filename_list[3][11])
+# print(filename_list[4][11])
+# print(filename_list[5][11])
+# print(filename_list[6][11])
+# print(filename_list[7][11])
